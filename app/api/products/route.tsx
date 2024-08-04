64016f298d081f28b8db396164a7a348aa6a4a4f
Mohammad Sulaiman <mohammad.sulaiman@vianoes.com>
2024-08-04 15:44:12 +0400

    crud user&product

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,18 +1,25 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest){
-    return NextResponse.json([
-        { id: 1, name: 'product 1', price: 2.5 },
-        { id: 2, name: 'product 2', price: 3.5 },
-    ]);
+export async function GET(request: NextRequest){
+    const users = await prisma.product.findMany();
+    return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest){
     const body = await request.json();
     const validation = schema.safeParse(body);
     if(!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 })
-    return NextResponse.json({ id: body.id, name: body.name, price: body.price }, { status: 201 });
+        return NextResponse.json(validation.error.errors, { status: 400 });
+
+    const newProduct = await prisma.product.create({
+        data: {
+          name: body.name,
+          price: body.price,
+        },
+    });
+
+    return NextResponse.json(newProduct, { status: 201 });
 }
