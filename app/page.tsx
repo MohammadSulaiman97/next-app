9d2775f1b95e19abb7dec14bc0429dcaf37f1d18
Mohammad Sulaiman <mohammad.sulaiman@vianoes.com>
2024-08-08 16:43:09 +0400

    lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,26 @@
-import Image from "next/image";
-import coffee from '@/public/images/coffee.jpg'
-import { Metadata } from "next";
+'use client';
+import { useState } from "react";
 
-export default async function Home() {
+
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(
+  () => import('./components/HeavyComponent'),
+    { 
+      // pre render make it false ssr
+      ssr: false,
+      loading: () => <p>Loading..</p> 
+    }
+);
+
+export default function Home() {
+  const [isVisible, setVisible]= useState(false);
   return (
-    <main className="relative h-screen">
-      <h1>Hello!!</h1>
-      {/* <Image 
-        src='https://bit.ly/react-cover'
-        alt="Coffee" 
-        fill
-        className="object-cover"
-        sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw, 33vw "
-        quality={100}
-      /> */}
+    <main>
+      <h1>Hello World!</h1>
+      <button onClick={() => setVisible(true)}>Show</button>
+      { isVisible && <HeavyComponent /> }
     </main>
   )
 }
 
 
-export async function generateMatedata(): Promise<Metadata>{
-  const product = await fetch('');
-  return {
-    title: 'product.title',
-    description: '..'
-  }
-}
